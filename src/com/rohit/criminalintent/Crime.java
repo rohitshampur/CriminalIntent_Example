e561f7eb39211f91d45e5db0f6359394a17a617b
stormtheh4ck3r <rohitshampur09@yahoo.com>
2014-12-21 20:09:32 +0530

    initial commit

---
+++ src/com/rohit/criminalintent/Crime.java
@@ -0,0 +1,12 @@
+package com.rohit.criminalintent;
+
+import java.util.UUID;
+
+public class Crime {
+	private UUID mId;
+	private String mTitle;
+	public Crime(){
+		mId = UUID.randomUUID();
+	}
+
+}
---
+++ src/com/rohit/criminalintent/CrimeFragment.java
@@ -0,0 +1,23 @@
+package com.rohit.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class CrimeFragment extends Fragment {
+	private Crime mCrime;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		mCrime = new Crime();
+	}
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_crime, parent, false);
+	}
+
+}
